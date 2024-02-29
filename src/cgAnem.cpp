a871dcd53af017163041e564dfbba73ff2835955
ElectroZheka <102234145+ElectroZheka@users.noreply.github.com>
2024-02-29 17:04:28 +0300

    Update cgAnem.cpp

--- src/cgAnem.cpp
+++ src/cgAnem.cpp
@@ -66,55 +66,6 @@ bool CG_Anem::register_write_byte(uint8_t regAddr, uint8_t regData)
     return true;
 }
 
-///*Get factory.*/
-//long CG_Anem::getFactory()
-//{
-//        uint8_t raw[4];
-//        if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_1, &raw[0]))
-//        {
-//            if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_2, &raw[1]))
-//            {
-//		if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_3, &raw[2]))
-//                {
-//	  	    if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_4, &raw[3]))
-//                    {
-//                        long factory =  (raw[0] << 24) | (raw[1] << 16) | (raw[2] << 8) | raw[3];
-//			return factory;
-//                    }
-//		}
-//	    }
-//        }
-//}
-
-///*Get factory_id_1.*/
-//uint8_t CG_Anem::getFactoryId1()
-//{
-//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_1), &_factoryid1);
-//    return _factoryid1;
-//}
-
-///*Get factory_id_2.*/
-//uint8_t CG_Anem::getFactoryId2()
-//{
-//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_2), &_factoryid2);
-//    return _factoryid2;
-//}
-
-///*Get factory_id_3.*/
-//uint8_t CG_Anem::getFactoryId3()
-//{
-//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_3), &_factoryid3);
-//    return _factoryid3;
-//}
-
-///*Get factory_id_4.*/
-//uint8_t CG_Anem::getFactoryId4()
-//{
-//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_4), &_factoryid4);
-//    return _factoryid4;
-//}
-//*/
-	
 /*Get chip id, default value: 0x11.*/
 uint8_t CG_Anem::getChipId()
 {
@@ -329,5 +280,55 @@ bool CG_Anem::disableWDT()
         if (register_write_byte(i2c_reg_STATUS, (uint8_t)0x0))
             return true;
     }
+
     return false;
 }
+
+///*Get factory.*/
+//long CG_Anem::getFactory()
+//{
+//        uint8_t raw[4];
+//        if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_1, &raw[0]))
+//        {
+//            if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_2, &raw[1]))
+//            {
+//		if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_3, &raw[2]))
+//                {
+//	  	    if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_4, &raw[3]))
+//                    {
+//                        long factory =  (raw[0] << 24) | (raw[1] << 16) | (raw[2] << 8) | raw[3];
+//			return factory;
+//                    }
+//		}
+//	    }
+//        }
+//}
+
+///*Get factory_id_1.*/
+//uint8_t CG_Anem::getFactoryId1()
+//{
+//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_1), &_factoryid1);
+//    return _factoryid1;
+//}
+
+///*Get factory_id_2.*/
+//uint8_t CG_Anem::getFactoryId2()
+//{
+//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_2), &_factoryid2);
+//    return _factoryid2;
+//}
+
+///*Get factory_id_3.*/
+//uint8_t CG_Anem::getFactoryId3()
+//{
+//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_3), &_factoryid3);
+//    return _factoryid3;
+//}
+
+///*Get factory_id_4.*/
+//uint8_t CG_Anem::getFactoryId4()
+//{
+//    register_read_byte(uint8_t(i2c_reg_FACTORY_ID_4), &_factoryid4);
+//    return _factoryid4;
+//}
+//*/
