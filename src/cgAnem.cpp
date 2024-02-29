805b9563f04b5a5004900eb5a28e22907f151207
ElectroZheka <102234145+ElectroZheka@users.noreply.github.com>
2024-02-29 15:50:47 +0300

    Update cgAnem.cpp

--- src/cgAnem.cpp
+++ src/cgAnem.cpp
@@ -78,7 +78,7 @@ long CG_Anem::getFactory()
                 {
 	  	    if (register_read_byte((uint8_t)i2c_reg_FACTORY_ID_4, &raw[3]))
                     {
-                        long tmp =  raw[0] << 24) | (raw[1] << 16) | (raw[2] << 8) | raw[3];
+                        long tmp =  (raw[0] << 24) | (raw[1] << 16) | (raw[2] << 8) | raw[3];
 			return tmp;
                     }
 		}
